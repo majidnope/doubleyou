5534661684e2c0d2db4d2a6d7db48b33e39dda17
majid-nope <muhammed0000majid@gmail.com>
2023-04-21 19:22:08 +0530

    blog

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,16 @@
 import Banner from "@/components/Banner/Banner";
 import Recent from "@/components/Recent/Recent";
+import RecentBlog from "@/components/RecentBlogs/RecentBlog";
 
 export default function Home() {
   return (
     <>
       <div className="main">
         <Banner />
-       
-        <Recent />
+        <div className="body">
+          <RecentBlog />
+          <Recent />
+        </div>
       </div>
     </>
   );
