326c1881fd2113b2c5dda180c84fce1929728b6b
majid-nope <muhammed0000majid@gmail.com>
2023-05-11 19:30:40 +0530

    fix:font

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,16 @@ import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <>
-
       <Head>
         <title>DoubleYou</title>
         <meta name="description" content="Enjoy the knowledge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MantineProvider
